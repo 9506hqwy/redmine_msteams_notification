1d30526803c9834bd82e57535b8424b0c69630fe
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-01-23 14:48:26 +0900

    不要な記載を削除した。

--- db/migrate/20220116014012_create_msteams_destinations.rb
+++ db/migrate/20220116014012_create_msteams_destinations.rb
@@ -9,7 +9,7 @@ end
 class CreateMsteamsDestinations < Migration
   def change
     create_table :msteams_destinations do |t|
-      t.belongs_to :project, null: false, foreign_key: true, dependent: :destroy
+      t.belongs_to :project, null: false, foreign_key: true
       t.string :url
       t.string :format
     end
