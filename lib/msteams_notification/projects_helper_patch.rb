d568818c34660c46e653ac336f6d6d2518acadf2
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-02-06 10:20:42 +0900

    Rails4 では prepend の代わりに alias_method_chain を使用する処理に変更した。

--- lib/msteams_notification/projects_helper_patch.rb
+++ lib/msteams_notification/projects_helper_patch.rb
@@ -2,7 +2,7 @@
 
 module RedmineMsteamsNotification
   module ProjectsHelperPatch
-    def project_settings_tabs
+    def msteams_notification_setting_tabs(tabs)
       action = {
         name: 'msteams_notification',
         controller: :msteams_destination,
@@ -11,11 +11,36 @@ module RedmineMsteamsNotification
         label: :msteams_notification,
       }
 
-      tabs = super
       tabs << action if User.current.allowed_to?(action, @project)
       tabs
     end
   end
+
+  module ProjectsHelperPatch4
+    include ProjectsHelperPatch
+
+    def self.included(base)
+      base.class_eval do
+        alias_method_chain(:project_settings_tabs, :msteams_notification)
+      end
+    end
+
+    def project_settings_tabs_with_msteams_notification
+      msteams_notification_setting_tabs(project_settings_tabs_without_msteams_notification)
+    end
+  end
+
+  module ProjectsHelperPatch5
+    include ProjectsHelperPatch
+
+    def project_settings_tabs
+      msteams_notification_setting_tabs(super)
+    end
+  end
 end
 
-ProjectsHelper.prepend RedmineMsteamsNotification::ProjectsHelperPatch
+if ActiveSupport::VERSION::MAJOR >= 5
+  ProjectsHelper.prepend RedmineMsteamsNotification::ProjectsHelperPatch5
+else
+  ProjectsHelper.include RedmineMsteamsNotification::ProjectsHelperPatch4
+end
